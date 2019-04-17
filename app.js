04e9633f848c1ca9ba9bd5b1afbd175a89891f54
eirenelakshita <laeirene59@gmail.com>
2019-04-17 01:12:10 -0400

    Commit the app

---
+++ app.js
@@ -0,0 +1,35 @@
+var rowNum = parseFloat(process.argv[2]);
+
+function generateArray(input) {
+  for (var i = 0; i <= input; i++) {
+    var newArr = [];
+    for (var j = 0; j <= i; j++) {
+      var num = factorial(i) / (factorial(j) * (factorial(i - j)))
+      newArr.push(num);
+    }
+    console.log(newArr);
+  }
+}
+  // function pascal(n) {
+  //   // var newArr = [];
+  //   for (var j = 0; j <= n; j++) {
+  //     var num = factorial(n) / (factorial(j) * (factorial(n - j)))
+  //     newArr.push(num);
+  //     console.log(newArr);
+  //   }
+  // }
+
+
+  function factorial(j) {
+    if (j == 0) {
+      return 1
+    } else {
+      return j * factorial(j - 1)
+    }
+  }
+
+  if (rowNum > 1) {
+    generateArray(rowNum);
+  } else {
+    console.log("Must enter number >= 1")
+  }
